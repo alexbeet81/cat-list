a26928855a3e57822ae750729f7580cfacb05400
Alex Pritchard <alexbeet1234@gmail.com>
2022-07-05 13:10:32 +0800

    creadted a hooks folder

--- src/pages/Breeds.js
+++ src/pages/Breeds.js
@@ -2,7 +2,11 @@ import classes from "./Breeds.module.css";
 import BreedCard from "../components/BreedCard";
 
 const Breeds = () => {
-  return <div><BreedCard/></div>
+  return (
+    <div>
+      <BreedCard />
+    </div>
+  );
 };
 
 export default Breeds;
