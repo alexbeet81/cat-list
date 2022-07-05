a0991de8c18702ec121fbbcc9bf6fbf46c49c110
Alex Pritchard <alexbeet1234@gmail.com>
2022-07-05 16:44:50 +0800

    adding details to modal cards

--- src/components/BreedModal.js
+++ src/components/BreedModal.js
@@ -1,8 +1,33 @@
+import noCatImage from "../assets/nocat.jpeg";
 import Modal from "./UI/Modal";
 import classes from "./BreedModal.module.css";
 
 const BreedModal = ({ onClick, breed }) => {
-  return <Modal onClose={onClick}>{breed.name}e</Modal>
+  console.log(breed);
+  let image = noCatImage;
+
+  if (breed.image != undefined) {
+    image = breed.image.url;
+  }
+
+  return (
+    <Modal onClose={onClick}>
+      <div className={classes.container}>
+        <div className={classes.image}>
+          <img src={image} alt={breed.name} />
+        </div>
+        <div className={classes.nameContainer}>
+          <h2>{breed.name}</h2>
+        </div>
+        <div className={classes.description}>
+          <p>{breed.description}</p>
+        </div>
+        <div className={classes.lifeSpan}>
+          <h4>Life span: {breed.life_span} years</h4>
+        </div>
+      </div>
+    </Modal>
+  );
 };
 
 export default BreedModal;
