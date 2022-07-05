74430def46ba6d50ac1488d0877a57fef23a2eba
Alex Pritchard <alexbeet1234@gmail.com>
2022-07-05 13:07:14 +0800

    updated BreedCard

--- src/components/BreedCard.js
+++ src/components/BreedCard.js
@@ -2,7 +2,13 @@ import classes from "./BreedCard.module.css";
 import Card from "./UI/Card";
 
 const BreedCard = () => {
-  return <Card className={classes.card}></Card>
+  return (
+    <Card className={classes.card}>
+      <div className={classes.breedName}>
+        <h2>Breed Name</h2>
+      </div>
+    </Card>
+  );
 };
 
 export default BreedCard;
