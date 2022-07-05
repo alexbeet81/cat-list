bf76eeb03aa7f17239abe2a31295eadc73905edc
Alex Pritchard <alexbeet1234@gmail.com>
2022-07-05 17:24:12 +0800

    added additional ratings

--- src/components/BreedModal.js
+++ src/components/BreedModal.js
@@ -1,3 +1,4 @@
+import RatingElement from './RatingElement';
 import noCatImage from "../assets/nocat.jpeg";
 import Modal from "./UI/Modal";
 import classes from "./BreedModal.module.css";
@@ -12,7 +13,7 @@ const BreedModal = ({ onClick, breed }) => {
 
   return (
     <Modal onClose={onClick}>
-      <div className={classes.container}>
+      <div className={classes.container} onClick={onClick}>
         <div className={classes.image}>
           <img src={image} alt={breed.name} />
         </div>
@@ -25,9 +26,12 @@ const BreedModal = ({ onClick, breed }) => {
         <div className={classes.lifeSpan}>
           <h4>Life span: {breed.life_span} years</h4>
         </div>
-        <div className={classes.stat}>
-          <h3>Lap friendly</h3>
-        </div>
+        <RatingElement name="Adaptability" rating={breed.adaptability}/>
+        <RatingElement name="Affection" rating={breed.affection_level}/>
+        <RatingElement name="Child Friendly" rating={breed.child_friendly}/>
+        <RatingElement name="Energy Level" rating={breed.energy_level}/>
+        <RatingElement name="Intelligence" rating={breed.intelligence}/>
+        <RatingElement name="Stranger friendly" rating={breed.stranger_friendly}/>
       </div>
     </Modal>
   );
