1793b756d48280abd0ffdbd5f7835a6b93546257
Alex Pritchard <alexbeet1234@gmail.com>
2022-07-05 15:43:16 +0800

    added gradient to image

--- src/components/BreedCard.js
+++ src/components/BreedCard.js
@@ -2,24 +2,25 @@ import classes from "./BreedCard.module.css";
 import Card from "./UI/Card";
 
 const BreedCard = ({ image, name }) => {
-
-  let fntSize = "1.5rem"
+  let fntSize = "1.5rem";
 
   if (name.length > 13) {
-    fntSize = "1.3rem"
+    fntSize = "1.3rem";
   }
 
   if (name.length > 15) {
-    fntSize = "1.2rem"
+    fntSize = "1.2rem";
   }
 
   if (name.length > 18) {
-    fntSize = "1rem"
+    fntSize = "1rem";
   }
 
   return (
     <Card className={classes.card}>
-      <img src={image} alt={name}/>
+      <div className={classes.gradient}>
+        <img src={image} alt={name} className={classes.overlay} />
+      </div>
       <div className={classes.breedName}>
         <h2 style={{ fontSize: fntSize }}>{name}</h2>
       </div>
