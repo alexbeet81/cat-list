d81951e806060a31c43328f84dc9d726ed245c94
Alex Pritchard <alexbeet1234@gmail.com>
2022-07-05 13:28:37 +0800

    wrote use get breeds

---
+++ src/hooks/use-get-breeds.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+import { baseUrl } from "../react-query/base-url";
+import { apiKey } from "../react-query/api-key";
+import { queryKeys } from "../react-query/query-keys";
+
+const getBreeds = async () => {
+  const { data } = await axios(`${baseUrl}/breeds`, {
+    headers: {
+      "x-api-key": apiKey,
+    },
+  });
+
+  return data;
+};
+
+export const useGetBreeds = () => {
+  const fallback = [];
+  const {
+    data = fallback,
+    isLoading,
+    isError,
+    error,
+  } = useQuery(queryKeys.breeds, getBreeds());
+
+  return { data, isLoading, isError, error };
+};
