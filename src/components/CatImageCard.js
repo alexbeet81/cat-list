b28f3584350df3baf4b9b9fb5bbea90893305064
Alex Pritchard <alexbeet1234@gmail.com>
2022-07-06 09:19:37 +0800

    added hearts to images

--- src/components/CatImageCard.js
+++ src/components/CatImageCard.js
@@ -1,9 +1,25 @@
+import { AiOutlineHeart, AiFillHeart, AiTwotoneHeart } from "react-icons/ai";
 import classes from "./CatImageCard.module.css";
 import Card from "./UI/Card";
+import { useState } from "react";
 
 const CatImageCard = ({ imageData }) => {
+  const [heartIsSelected, setHeartIsSelected] = useState(false);
+
+  const selectHeartHandler = () => {
+    setHeartIsSelected((prevState) => !prevState);
+  };
+
+  const heartClasses = heartIsSelected
+    ? `${classes.heart} ${classes.selected}`
+    : classes.heart;
+  const heartElement = heartIsSelected ? <AiFillHeart /> : <AiTwotoneHeart />;
+
   return (
     <Card className={classes.card}>
+      <div className={heartClasses} onClick={selectHeartHandler}>
+        {heartElement}
+      </div>
       <div className={classes.image}>
         <img src={imageData.url} alt="image" />
       </div>
